397255333fa4c5abab00437d72cd2dbe47e2b505
Dayne May <dmay@aio-tv.com>
2017-02-05 00:56:03 -0400

    Address review feedback
    * Allow only alphanumerics, hyphen, underscore in filenames (excluding extension)
    * Define and use exit_with_msg() function

--- add_gif.sh
+++ add_gif.sh
@@ -1,45 +1,34 @@
 #!/usr/bin/env bash
 
-USAGE="Usage: ./add_gif.sh name http://url_of_the_gif"
+USAGE="Usage: $0 name http://url_of_the_gif"
+NAME_PATTERN=^[a-zA-Z0-9_-]+$
 NAME=$1
 URL=$2
 
-# Usage 
-if [[ "$NAME" == "--help" ]]; then
-    echo $USAGE
-    exit 0
-fi
 
-if [[ $# -ne 2 ]]; then
-    echo $USAGE
-    exit 1
-fi
+function exit_with_msg
+{
+    echo "$2" 1>&2
+    exit $1
+}
+
+
+# Usage 
+[[ "$NAME" != "--help" ]] || exit_with_msg 0 "$USAGE"
+[[ $# -eq 2 ]] || exit_with_msg 1 "$USAGE" 
 
 # Ensure availability of wget
-if ! type wget > /dev/null 2> /dev/null; then
-    echo "Script requires wget"
-    exit 2
-fi
+type wget > /dev/null 2> /dev/null || exit_with_msg 2 "Script requires wget"
 
-# Ensure name is not already in use
-if [[ -f ./_orig/$NAME.gif ]]; then
-    echo "The name $NAME is already in use (see ./_orig/$NAME.gif)"
-    exit 3
-fi
+# Ensure NAME is correctly formed
+[[ $NAME =~ $NAME_PATTERN ]] || exit_with_msg 3 "The name \"$NAME\" must match the regex \"$NAME_PATTERN\""
+
+# Ensure NAME is not already in use
+[[ ! -f "./_orig/$NAME.gif" ]] || exit_with_msg 4 "The name \"$NAME\" is already in use (see ./_orig/$NAME.gif)"
 
 # Get the new GIF
-wget $URL -O ./_orig/$NAME.gif
-
-if [[ $? -ne 0 ]]; then
-    echo "Problem getting GIF"
-    exit 4
-fi
+wget "$URL" -O "./_orig/$NAME.gif" || exit_with_msg 5 "Problem getting GIF"
 
 # Run ./process_gifs.sh
-./process_gifs.sh
-
-if [[ $? -ne 0 ]]; then
-    echo "Problem processing GIF"
-    exit 5
-fi
+./process_gifs.sh || exit_with_msg 6 "Problem processing GIF"
 
